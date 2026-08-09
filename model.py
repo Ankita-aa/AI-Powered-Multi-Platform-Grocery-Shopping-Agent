4d175493895b78793136c386f2cc21da4c2f8647
Ankita Upadhyay <ankita.upadhyay.it19@gmail.com>
2026-08-09 15:13:31 +0530

    Add work for feature/my-branch

--- model.py
+++ model.py
@@ -1,13 +1,6 @@
 import os
-
 from dotenv import load_dotenv
-
-from langchain_huggingface import (
-    ChatHuggingFace,
-    HuggingFaceEndpoint,
-)
-
-# from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 
 load_dotenv()
 
@@ -15,49 +8,15 @@ _llm = None
 
 
 def get_llm():
-
     global _llm
 
     if _llm:
         return _llm
 
-    provider = os.getenv(
-        "MODEL_PROVIDER",
-        "huggingface"
+    _llm = ChatGroq(
+        model_name="llama-3.1-8b-instant",
+        temperature=0,
+        api_key=os.getenv("GROQ_API_KEY"),
     )
-
-    # if provider == "huggingface":
-
-    endpoint = HuggingFaceEndpoint(
-
-        repo_id=os.getenv(
-            "HF_MODEL",
-            "Qwen/Qwen2.5-7B-Instruct"
-        ),
-
-        huggingfacehub_api_token=os.getenv(
-            "HUGGINGFACEHUB_API_TOKEN"
-        ),
-
-        temperature=0.2,
-
-        max_new_tokens=1024
-
-    )
-
-    _llm = ChatHuggingFace(
-       llm=endpoint
-    )
-
-    # else:
-
-    #     _llm = ChatOllama(
-
-    #         model=os.getenv(
-    #             "OLLAMA_MODEL",
-    #             "qwen2.5:7b"
-    #         )
-
-    #     )
 
     return _llm
